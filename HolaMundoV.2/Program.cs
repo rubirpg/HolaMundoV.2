f49e3474dc3f711bea3035ca78b47d55f48755a3
Portatil <Portatil@DESKTOP-EH59PCK>
2022-06-10 13:42:07 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundoV.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("HOLA MUNDO!!!");
+            int num1 = 5;
+            int nuero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
